7d70b9bac363d689fed97ecfb19d14347e51ce36
alexor98 <a.ornbratt@gmail.com>
2019-04-15 13:03:25 +0200

    Added a simple register view

--- app/src/main/java/com/bulbasaur/dat256/viewmodel/RegisterActivity.java
+++ app/src/main/java/com/bulbasaur/dat256/viewmodel/RegisterActivity.java
@@ -1,13 +1,17 @@
 package com.bulbasaur.dat256.viewmodel;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.bulbasaur.dat256.R;
+import com.bulbasaur.dat256.model.Country;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,16 +19,18 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        Spinner phoneNumberSpinner = findViewById(R.id.phoneNumberSpinner);
+        ArrayList countryList = new ArrayList<>();
+        Arrays.asList(Country.values()).forEach(x -> countryList.add(x.countryCode));
+        ArrayAdapter<String> phoneNumberSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, countryList);
+
+        phoneNumberSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        phoneNumberSpinner.setAdapter(phoneNumberSpinnerAdapter);
+
+        Button createAccountButton = findViewById(R.id.createAccountButton);
+        createAccountButton.setOnClickListener(v -> {
+
         });
     }
 }
