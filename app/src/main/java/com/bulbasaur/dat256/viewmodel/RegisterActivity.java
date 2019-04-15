8827190268869f260e4ed5a3ec7bb39936f6bf28
hasse98 <jaberisme@gmail.com>
2019-04-15 14:54:04 +0200

    Started work on error messages for textboxes, handing off to Alex

--- app/src/main/java/com/bulbasaur/dat256/viewmodel/RegisterActivity.java
+++ app/src/main/java/com/bulbasaur/dat256/viewmodel/RegisterActivity.java
@@ -2,13 +2,17 @@ package com.bulbasaur.dat256.viewmodel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.bulbasaur.dat256.R;
 import com.bulbasaur.dat256.model.Country;
+import com.bulbasaur.dat256.model.Validator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,6 +35,26 @@ public class RegisterActivity extends AppCompatActivity {
         Button createAccountButton = findViewById(R.id.createAccountButton);
         createAccountButton.setOnClickListener(v -> {
 
+        });
+
+        EditText firstNameEditText = findViewById(R.id.firstNameEditText);
+        firstNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!Validator.detectSpecialChar(firstNameEditText.getText().toString())) {
+                    firstNameEditText.//set other style
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
         });
     }
 }
