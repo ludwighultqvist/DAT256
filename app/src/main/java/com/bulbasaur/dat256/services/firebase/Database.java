7b7d6a2e2bbd370932e659a7c8a9ce79e54ebf5b
Ludwig Hultqvist <ludhul@student.chalmers.se>
2019-04-23 17:55:27 +0200

    Commented on database files

--- app/src/main/java/com/bulbasaur/dat256/services/firebase/Database.java
+++ app/src/main/java/com/bulbasaur/dat256/services/firebase/Database.java
@@ -1,21 +1,29 @@
 package com.bulbasaur.dat256.services.firebase;
 
 import android.app.Activity;
-import android.util.Log;
-
 import com.bulbasaur.dat256.services.Database.Authenticator;
 import com.bulbasaur.dat256.services.Database.PhoneAuthenticator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+/**
+ * @author ludwighultqvist
+ * class that acts as as the main entry point for fetching and updating the Firestore database.
+ * the class is a singleton so that it is synchroniced when multiple objects needs access
+ */
 public class Database {
+
     private static final String USERS = "users";
     private static final String MEETUPS = "meetups";
     private static final String GROUPS = "groups";
-
     private static Database instance;
+
     private Authenticator authenticator;
 
+    /**
+     * initializes, if necessary, the singleton Database object and returns it
+     * @return the singleton object
+     */
     public static Database getInstance() {
         if (instance == null) {
             instance = new Database();
@@ -24,29 +32,63 @@ public class Database {
         return instance;
     }
 
+    /**
+     * private constructor, needed for the class to be a singleton
+     */
     private Database() {}
 
+    /**
+     * creates a new PhoneAuthenticator object, stores in the Database object and returns it
+     * @param activity the activity required by the authenticator
+     * @return the PhoneAuthenticator object
+     */
     public Authenticator phoneAuthenticator(Activity activity) {
         authenticator = new PhoneAuthenticator(activity);
         return authenticator;
     }
 
+    /**
+     * returns the active authenticator, it can only be one active
+     * @return the Authenticator object
+     */
     public Authenticator activeAuthenticator() {
         return authenticator;
     }
 
+    /**
+     * creates and returns a reference to the databases collection of users
+     * @return the DBCollection object
+     */
     public DBCollection users() {
         return new Collection(USERS);
     }
 
+    /**
+     * creates and returns a reference to the databases collection of meetups
+     * @return the DBCollection object
+     */
     public DBCollection meetups() {
         return new Collection(MEETUPS);
     }
 
+
+    /**
+     * creates and returns a reference to the databases collection of groups
+     * @return the DBCollection object
+     */
     public DBCollection groups() {
         return new Collection(GROUPS);
     }
 
+    /**
+     * returns the currently logged in user.
+     * if no user is logged in null will be returned.
+     * if a user is logged in and it is already in the database, the reference to the users document
+     * is returned.
+     * if a user is logged in but not saved in the database, a new user document will be created
+     * with the logged in users uid
+     * @return the DBDocument or null
+     */
     public DBDocument user() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null) return null;
--- app/src/main/java/com/bulbasaur/dat256/services/firebase/Document.java
+++ app/src/main/java/com/bulbasaur/dat256/services/firebase/Document.java
@@ -1,29 +1,45 @@
 package com.bulbasaur.dat256.services.firebase;
 
 import android.util.Log;
-
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.SetOptions;
-
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author ludwighultqvist
+ * class that implements the DBDocument interface
+ */
 class Document implements DBDocument {
 
     private DocumentReference document;
     private Map<String, Object> data = new HashMap<>();
 
+    /**
+     * package private constructor which creates a new document with the given reference to a
+     * document in the Firestore database
+     * @param document the DocumentReference object
+     */
     Document(DocumentReference document) {
         if (document != null) {
             init(document);
         }
     }
 
+    /**
+     * package private constructor which creates a new empty document with no reference to a
+     * document in the Firestore database
+     */
     Document() {
         this(null);
     }
 
+    /**
+     * initializes the document object with the given reference to a document in the Firestore
+     * database and fetches the data from it and stores it in the Map object
+     * @param document the given DocumentReference object
+     */
     void init(DocumentReference document) {
         this.document = document;
         document.get()
@@ -41,21 +57,39 @@ class Document implements DBDocument {
                 .addOnFailureListener(e -> {});
     }
 
+    /**
+     * returns the id of the document
+     * @return the id string
+     */
     @Override
     public String id() {
         return document.getId();
     }
 
+    /**
+     * gets the object of the given field. if that field or object does not exist, null
+     * is returned
+     * @param field the field string
+     * @return the object of the field
+     */
     @Override
     public Object get(String field) {
         return data.get(field.toLowerCase());
     }
 
+    /**
+     * gets the object of the given field.
+     * @param field the field string
+     * @param object the object to be saved on the field
+     */
     @Override
     public void set(String field, Object object) {
         data.put(field.toLowerCase(), object);
     }
 
+    /**
+     * saves the stored objects of the fields in the Firestore database
+     */
     @Override
     public void save() {
         document.set(data, SetOptions.merge())
@@ -65,11 +99,9 @@ class Document implements DBDocument {
                 .addOnFailureListener(e -> {});
     }
 
-    @Override
-    public DBCollection subCollection(String name) {
-        return new Collection(document.collection(name));
-    }
-
+    /**
+     * deletes the document from the Firestore database
+     */
     @Override
     public void delete() {
         document.delete()
@@ -77,8 +109,22 @@ class Document implements DBDocument {
                 .addOnFailureListener(e -> {});
     }
 
+    /**
+     * returns a boolean if the document does not exist in the database
+     * @return the boolean value
+     */
     @Override
     public boolean isEmpty() {
         return document == null;
+    }
+
+    /**
+     * fetches a subCollection of the document with the name, e.g. the groups of a user etc.
+     * @param name the name string
+     * @return the DBCollection object
+     */
+    @Override
+    public DBCollection subCollection(String name) {
+        return new Collection(document.collection(name));
     }
 }
