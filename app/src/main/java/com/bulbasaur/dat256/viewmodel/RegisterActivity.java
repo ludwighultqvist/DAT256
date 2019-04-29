41fde8e96aca042d5679270fcf4ca09ecc500d09
johanmejborn <johanmejborn@outlook.com>
2019-04-29 16:00:11 +0200

    Terms and Conditions dummy view fixed. Checkbox for accepting terms before creating account fixed. RegisterView updated with the new buttons.

--- app/src/main/java/com/bulbasaur/dat256/viewmodel/RegisterActivity.java
+++ app/src/main/java/com/bulbasaur/dat256/viewmodel/RegisterActivity.java
@@ -8,6 +8,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 
 import com.bulbasaur.dat256.R;
@@ -22,7 +23,9 @@ import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private Button createAccountButton;
+    private Button createAccountButton, readTerms;
+    private CheckBox checkBox;
+
 
     private boolean firstNameValid = false, lastNameValid = false, phoneNumberValid = false;
 
@@ -49,6 +52,8 @@ public class RegisterActivity extends AppCompatActivity {
         phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
         createAccountButton = findViewById(R.id.createAccountButton);
         Button goToLoginViewButton = findViewById(R.id.goToLoginViewButton);
+        readTerms = findViewById(R.id.readTermsButton);
+        checkBox = findViewById(R.id.termsCheckbox);
 
         //Updates the selected country code to the correct value
         phoneNumberSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -59,7 +64,16 @@ public class RegisterActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {}
         });
 
-        //Sets up the validation code for the first name text field
+        readTerms.setOnClickListener(v -> {
+                    startActivity(new Intent(RegisterActivity.this, Terms_And_Conditions_Activity.class));
+                });
+        checkBox.setOnClickListener(v -> {
+            checkBox.isChecked();
+            updateCreateAccountButton();
+        });
+
+
+          //Sets up the validation code for the first name text field
         firstNameEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
@@ -135,8 +149,10 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+
+
     private void updateCreateAccountButton() {
-        if (firstNameValid && lastNameValid && phoneNumberValid) {
+        if (firstNameValid && lastNameValid && phoneNumberValid && checkBox.isChecked()) {
             createAccountButton.setEnabled(true);
         } else {
             createAccountButton.setEnabled(false);
