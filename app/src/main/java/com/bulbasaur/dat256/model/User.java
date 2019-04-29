f650e0abc648ba302d1c08853693c30dffb2e44d
Shoulder <axesven@student.chalmers.se>
2019-04-29 14:49:30 +0200

    Changed that first view is where u are, added some attributes to meetUp and users

--- app/src/main/java/com/bulbasaur/dat256/model/User.java
+++ app/src/main/java/com/bulbasaur/dat256/model/User.java
@@ -1,14 +1,24 @@
 package com.bulbasaur.dat256.model;
 
+import java.util.ArrayList;
+
 public class User {
     private String firtName;
     private String lastName;
     private String phoneNumber;
     Country country;
+    private int score;
+    private ArrayList<MeetUp> createdMeetUps = new ArrayList<>();
+    private ArrayList<MeetUp> joinedMeetUps = new ArrayList<>();
+    private Coordinates coordinates;
 
     public User(String firstName, String lastName, String phoneNumber){
         this.firtName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
+        score = 0;
+
     }
+
+
 }
