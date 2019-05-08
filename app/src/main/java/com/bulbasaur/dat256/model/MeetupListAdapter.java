a76f8a261b4cb60138e0a8cc0ed57328b7700e26
Matthias Baumgartner <matthiasandbaum@gmail.com>
2019-05-08 10:40:11 +0200

    Hårdkodad lista på MeetUps, la till en knapp i hamburgarmenyn för att nå vyn

---
+++ app/src/main/java/com/bulbasaur/dat256/model/MeetupListAdapter.java
@@ -0,0 +1,46 @@
+package com.bulbasaur.dat256.model;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.bulbasaur.dat256.R;
+
+import java.util.ArrayList;
+
+
+public class MeetupListAdapter extends ArrayAdapter<MeetUp> {
+    private Context mContext;
+    int mResource;
+
+    public MeetupListAdapter(Context context, int resource, ArrayList<MeetUp> objects) {
+        super(context, resource, objects);
+        this.mContext = context;
+        mResource= resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position,  View convertView,  ViewGroup parent) {
+        String name = getItem(position).getName();
+        String desc = getItem(position).getDescription();
+
+        MeetUp meetup = new MeetUp(name, desc);
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        convertView = inflater.inflate(mResource, parent, false);
+
+        TextView tvName =(TextView) convertView.findViewById(R.id.meetupName);
+        TextView tvDesc =(TextView) convertView.findViewById(R.id.meetupDesc);
+
+        tvName.setText(name);
+        tvDesc.setText(desc);
+
+        return convertView;
+    }
+}
+
--- app/src/main/java/com/bulbasaur/dat256/viewmodel/ListActivity.java
+++ app/src/main/java/com/bulbasaur/dat256/viewmodel/ListActivity.java
@@ -2,24 +2,52 @@ package com.bulbasaur.dat256.viewmodel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bulbasaur.dat256.R;
+import com.bulbasaur.dat256.model.Coordinates;
+import com.bulbasaur.dat256.model.MeetUp;
+import com.bulbasaur.dat256.model.MeetupListAdapter;
 import com.bulbasaur.dat256.services.firebase.Database;
+
+import java.util.ArrayList;
+
 
 public class ListActivity extends AppCompatActivity {
     private ListView meetupList;
-
+    private ArrayList<MeetUp> meetUps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meetup_list);
         meetupList = (ListView) findViewById(R.id.listView);
+        meetUps = new ArrayList<>();
+        init();
 
     }
+
+    private void init() {
+        meetUps.add(new MeetUp("100","Baum", "Fest Hos Baum", new Coordinates(57.704330, 11.963180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("101","Nils", "Fest Hos Nils", new Coordinates(57.704330, 11.973180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("102","Mejborn", "Fest Hos Mejborn", new Coordinates(57.704330, 11.983180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("103","Baum", "Fest Hos Baum", new Coordinates(57.704330, 11.963180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("104","Nils", "Fest Hos Nils", new Coordinates(57.704330, 11.973180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("105","Mejborn", "Fest Hos Mejborn", new Coordinates(57.704330, 11.983180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("106","Baum", "Fest Hos Baum", new Coordinates(57.704330, 11.963180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("107","Nils", "Fest Hos Nils", new Coordinates(57.704330, 11.973180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("108","Mejborn", "Fest Hos Mejborn", new Coordinates(57.704330, 11.983180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("109","Baum", "Fest Hos Baum", new Coordinates(57.704330, 11.963180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("110","Nils", "Fest Hos Nils", new Coordinates(57.704330, 11.973180), "fet fest BYOB"));
+        meetUps.add(new MeetUp("111","Mejborn", "Fest Hos Mejborn", new Coordinates(57.704330, 11.983180), "fet fest BYOB"));
+
+        MeetupListAdapter adapter = new MeetupListAdapter(this, R.layout.activity_meetuplistobject, meetUps);
+        meetupList.setAdapter(adapter);
+    }
+
 
 /* Todo: Create two dummy meetup objects and fill with the information we want. (Name and Date/time)
     Create ArrayList with Meetup-objects
