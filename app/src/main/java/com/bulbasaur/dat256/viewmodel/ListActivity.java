993dbabd099f3c14064d6a210fb07bdcd5d9858e
Nils <fittnippe@gmail.com>
2019-05-09 11:03:10 +0200

    Fixed so that the listobjects are clickable and sends the user to the right meetupview

--- app/src/main/java/com/bulbasaur/dat256/viewmodel/ListActivity.java
+++ app/src/main/java/com/bulbasaur/dat256/viewmodel/ListActivity.java
@@ -1,8 +1,12 @@
 package com.bulbasaur.dat256.viewmodel;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,9 +38,21 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meetup_list);
         meetupList = (ListView) findViewById(R.id.listView);
-        //meetUps = new ArrayList<>();
+        meetupList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                              @Override
+                                              public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                  Intent meetUpIntent = new Intent(ListActivity.this, MeetUpActivity.class);
+                                                  startActivity(meetUpIntent.putExtra("MeetUp", getMeetUp(position)));
+                                                  setResult(Activity.RESULT_OK, meetUpIntent);
+                                              }
+                                          }
+        );
         init();
 
+    }
+
+    private MeetUp getMeetUp(int pos){
+        return meetUps.get(pos);
     }
 
     private void init() {
